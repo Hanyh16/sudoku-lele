862d0e950ed38c4c51ce1c25e1fb3b3d0fe99a1e
qjj <1823407638@qq.com>
2025-12-27 16:02:41 +0800

    移除无用的笔记按键

--- src/components/Controls/ActionBar/Actions.svelte
+++ src/components/Controls/ActionBar/Actions.svelte
@@ -112,13 +112,13 @@
 		{/if}
 	</button>
 
-	<button class="btn btn-round btn-badge" on:click={notes.toggle} title="Notes ({$notes ? 'ON' : 'OFF'})">
+	<!-- <button class="btn btn-round btn-badge" on:click={notes.toggle} title="Notes ({$notes ? 'ON' : 'OFF'})">
 		<svg class="icon-outline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
 		</svg>
 
 		<span class="badge tracking-tighter" class:badge-primary={$notes}>{$notes ? 'ON' : 'OFF'}</span>
-	</button>
+	</button> -->
 
 </div>
 
